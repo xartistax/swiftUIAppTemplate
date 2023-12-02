094a6a502783953fbbfb8675d56a9d0e6cdd58b5
Demian Füglistaler <demianfueglistaler@icloud..com>
2023-12-02 01:46:07 +0100

    Initial Commit

---
+++ AppTemplate/AppTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTemplateApp.swift
+//  AppTemplate
+//
+//  Created by Demian Füglistaler on 02.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppTemplate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppTemplate
+//
+//  Created by Demian Füglistaler on 02.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
